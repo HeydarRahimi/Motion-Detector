56fb3cbb5d635837bcbac910074e93a9af65fedf
M_R <cmr100199@gmail.com>
2023-12-22 01:56:28 +0330

    Construction of AI laboratory project

---
+++ M_D.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+def get_frame_gray(cap):
+    ret, frame = cap.read()
+    frame = frame[:,130:550]
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray,(21,21)0)
+    return frame, gray
