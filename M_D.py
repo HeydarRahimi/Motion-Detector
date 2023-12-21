6e5cf166f7dfad1eeabaa7ff56ffb04fa7eb90bc
M_R <cmr100199@gmail.com>
2023-12-22 03:13:22 +0330

    Function to detect and limit objects and their edges in the image

--- M_D.py
+++ M_D.py
@@ -7,6 +7,10 @@ def get_frame_gray(cap):
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray,(21,21)0)
     return frame, gray
-
-
-
+def bounding_rect(frame,thresh):
+    kernel = np.ones((5,5), np.uint8)
+    thresh = cv2.dilate(thresh,kernel,iterations=5)
+    cnts,_ =cv2.findContours(thresh.copy(),
+                             cv2.RETR_EXTERAL,
+                             cv2.CHAIN_APPROX_SIMPLE)
+    
